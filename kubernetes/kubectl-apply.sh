c5af22d0157c45d81c758c9e79bbb301eeadb124
Deepu K Sasidharan <d4udts@gmail.com>
2018-09-12 12:36:18 +0200

    create kubernetes files

---
+++ kubernetes/kubectl-apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+kubectl apply -f registry/
+kubectl apply -f invoice/
+kubectl apply -f notification/
+kubectl apply -f store/
+kubectl apply -f console/
