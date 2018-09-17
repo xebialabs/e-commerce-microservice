df301104854ad1977994cb82d79b74e0da3440c9
eltonkevani <eqevani@xebialabs.com>
2018-09-17 12:15:04 +0200

    change namespace

--- xl-platform/kubernetes/kubectl-apply.sh
+++ xl-platform/kubernetes/kubectl-apply.sh
@@ -2,6 +2,7 @@
 # Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
 # Usage: bash kubectl-apply.sh
 
+kubectl apply -f namespace.yml
 kubectl apply -f registry/
 kubectl apply -f invoice/
 kubectl apply -f notification/
