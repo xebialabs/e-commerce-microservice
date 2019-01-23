48af765b4211dfc5a1773deb6dfefad1f886e1e3
Deepu K Sasidharan <d4udts@gmail.com>
2019-01-23 16:14:30 +0100

    add kubernetes setup

---
+++ kubernetes/kubectl-apply.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Files are ordered in proper order with needed wait for the dependent custom resource definitions to get initialized.
+# Usage: bash kubectl-apply.sh
+
+kubectl apply -f namespace.yml
+kubectl apply -f registry/
+kubectl apply -f store/
+kubectl apply -f invoice/
+kubectl apply -f notification/
